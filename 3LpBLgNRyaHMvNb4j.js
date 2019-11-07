8d922ee8054bdac40361af54ddc7f65fee67442a
juliafealves <giT#8985>
2019-11-07 13:47:08 -0300

    Challenge: Return the Sum of Two Numbers

---
+++ 3LpBLgNRyaHMvNb4j.js
@@ -0,0 +1,10 @@
+/**
+ * Return the Sum of Two Numbers
+ * https://edabit.com/challenge/3LpBLgNRyaHMvNb4j #very-easy
+ * @param a
+ * @param b
+ * @returns {*}
+ */
+function addition(a, b) {
+  return a + b;
+}
--- ARr5tA458o2tC9FTN.js
+++ ARr5tA458o2tC9FTN.js
@@ -1,6 +1,6 @@
 /**
  * How Edabit Works
- * https://edabit.com/challenge/ARr5tA458o2tC9FTN
+ * https://edabit.com/challenge/ARr5tA458o2tC9FTN #very-easy
  * @returns {string}
  */
 function hello() {
